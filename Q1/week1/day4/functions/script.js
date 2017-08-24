40f86e02fbd7bb278e44da6ad3c8a1c563ae10da
bpaksoy <paksoybanu83@gmail.com>
2017-08-24 11:37:16 -0400

    Add return

--- Q1/week1/day4/functions/script.js
+++ Q1/week1/day4/functions/script.js
@@ -1,6 +1,14 @@
 
 function personalVirus(myName){
   console.log("Your machine is infected, " + myName + "!!!");
+  return "Hi Mom";
 }
 personalVirus("Banu");
 personalVirus("Jen");
+
+function yourName(firstName, lastName){
+ return firstName + " " + lastName;
+}
+
+var fullName = yourName("Banu", "Yilmaz");
+var user2 = yourName("Bilge", "Yilmaz");

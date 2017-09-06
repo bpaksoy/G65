ccfb7ad4cebd2c4d1f81caba17b4b7d3fe56491f
bpaksoy <paksoybanu83@gmail.com>
2017-09-06 16:24:22 -0400

    week3 added

---
+++ Q1/week3/event_review/script.js
@@ -0,0 +1,19 @@
+var redBox = document.querySelector("#red");
+redBox.addEventListener("click", function(){
+  document.querySelector("body").style.backgroundColor= "red";
+});
+
+var blueBox = document.querySelector("#blue");
+blueBox.addEventListener("click", function(){
+  document.querySelector("body").style.backgroundColor = "blue";
+});
+
+var greenBox = document.querySelector("#green");
+greenBox.addEventListener("click", function(){
+  document.querySelector("body").style.backgroundColor = "green";
+});
+
+var yellowBox = document.querySelector("#yellow");
+yellowBox.addEventListener("click", function(){
+  document.querySelector("body").style.backgroundColor = "yellow";
+});
